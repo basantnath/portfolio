63a2f701995d02f34a4e774a80e182e87bb47a0c
I'm Basant <basantnath00@gmail.com>
2024-11-12 22:25:32 +0545

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,12 +4,12 @@ export default function About() {
       <h2 className="mb-2 heading pro-con-heading text-highlight">About Me</h2>
       <p className="pro-con-cont text-highlight">
         Hello again! I'm Basant,{" "}
-        <span className="underline text-gray-900 text-highlight">
+        <span className="underline text-gray-930 text-highlight">
           currently a 4th semester student
         </span>{" "}
         in BSc CSIT. I'm from Nepal, and I absolutely love coding. My passion
         for programming is driven by creativity, and I'm{" "}
-        <span className="underline text-gray-900 text-highlight">
+        <span className="underline text-gray-930 text-highlight">
           good at solving problems
         </span>
         . And of course, I code for pizza! I'm always excited to learn new
